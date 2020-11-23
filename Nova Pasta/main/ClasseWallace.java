a67e2e2f784f3eb4b40f74e3dbd4a83f8672a414
meunomeehJoao <jvitorcoc54@gmail.com>
2020-11-23 18:18:35 -0200

    projeto com nome

--- Nova Pasta/main/ClasseWallace.java
+++ Nova Pasta/main/ClasseWallace.java
@@ -4,5 +4,6 @@ public class ClasseWallace {
     public static void main(String[] args) {
         System.out.println("Wallace Castilho Martin");
         System.out.println("Leticia Romão Bettamio Duarte"); 
+        System.out.println("João Vitor Lopes Ferreira");
     }
 }
