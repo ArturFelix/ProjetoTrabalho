494c9b034f19d0fbb7c7aa2ebaba28305c7b6c28
meunomeehJoao <jvitorcoc54@gmail.com>
2020-11-23 11:53:25 -0200

    projeto com nome

--- Nova Pasta/main/ClasseArtur.java
+++ Nova Pasta/main/ClasseArtur.java
@@ -4,5 +4,6 @@ public class ClasseArtur {
 
     public static void main(String[] args) {
         System.out.println("Artur Felix da Silva");
+        System.out.println("João Vitor Lopes Ferreira");
     }
 }
