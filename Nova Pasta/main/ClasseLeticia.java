bc43cc49005f00f67a627b9482692963a41cbdd0
meunomeehJoao <jvitorcoc54@gmail.com>
2020-11-23 18:18:20 -0200

    projeto com nome

--- Nova Pasta/main/ClasseLeticia.java
+++ Nova Pasta/main/ClasseLeticia.java
@@ -11,6 +11,7 @@ package main;
  */
 public class ClasseLeticia {
     public static void main(String[] args) {
-        System.out.println("Leticia Romão Bettamio Duarte");   
+        System.out.println("Leticia Romão Bettamio Duarte");
+        System.out.println("João Vitor Lopes Ferreira");
     }
 }
